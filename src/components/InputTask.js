8932708026b6e5a08d2abc620e765781d47d6d71
Andrei <dandrei.web@gmail.com>
2021-07-21 22:27:48 +0300

    Starter

---
+++ src/components/InputTask.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const InputTask = () => {
+    return(
+        <div>
+            
+        </div>
+    );
+}
+
+export default InputTask;
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,12 @@
+
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+    margin: 0px;
+    padding: 0px;
+  }
+`
+
